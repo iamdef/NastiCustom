42a1f276471438a34b1783c0285c25ac2ce32d7d
Dmitry <xolodetzzz@yandex.ru>
2022-04-13 19:09:34 +0300

    Added all project files

---
+++ README.md
@@ -0,0 +1,14 @@
+
+# NastiCustom
+
+Web page of the clothing artist
+
+## Authors
+
+- [@iamdef](https://github.com/iamdef)
+
+
+## Screenshots
+
+![App Screenshot](https://i.ibb.co/smg04ZG/Nasti-Custom.png)
+
---
+++ fullpage.js
@@ -0,0 +1,9 @@
+new fullpage('#fullpage', {
+    //options here
+    autoScrolling:true,
+    scrollHorizontally: true,
+    anchors:['firstPage', 'secondPage', 'thirdPage', 'fourthPage']
+});
+
+//methods
+fullpage_api.setAllowScrolling(true);
